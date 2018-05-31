14da6d50c676946bf73162186d1f0aaf4b05d26e
retto710 <31112329+retto710@users.noreply.github.com>
2018-05-31 18:24:53 -0500

    v1.1

--- app/src/main/java/com/example/anthony/a20/RegisterActivity.java
+++ app/src/main/java/com/example/anthony/a20/RegisterActivity.java
@@ -1,25 +1,46 @@
 package com.example.anthony.a20;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class RegisterActivity extends AppCompatActivity {
     public static final int PICK_IMAGE = 1;
+    private FirebaseAuth mAuth;
+    EditText editTextEmail;
+    EditText editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ImageView img_profile = findViewById(R.id.img_profile);
         Button btn_save = findViewById(R.id.btn_save);
+        editTextEmail = findViewById(R.id.edt_Correo);
+        editTextPassword= findViewById(R.id.edt_Contraseña);
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(getApplicationContext(),ChooseProfileActivity.class);
-                startActivity(intent);
+                signIn(editTextEmail.getText().toString(),editTextPassword.getText().toString());
             }
         });
         img_profile.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +53,56 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        editTextEmail = findViewById(R.id.edt_Correo);
+        editTextPassword= findViewById(R.id.edt_Contraseña);
+        String email = editTextEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Required.");
+            valid = false;
+        } else {
+            editTextEmail.setError(null);
+        }
+        String password = editTextPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Required.");
+            valid = false;
+        } else {
+            editTextPassword.setError(null);
+        }
+        return valid;
+    }
+
+    private void signIn(String email, String password) {
+        Log.d("login", "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("d", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(), ChooseProfileActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("d", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+
 }
