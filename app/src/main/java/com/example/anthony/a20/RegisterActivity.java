7c7f32f81655af408f34cb9e3dc71cc4c3fd574a
retto710 <retto710@users.noreply.github.com>
2018-06-01 11:08:04 -0500

    v.1.2

--- app/src/main/java/com/example/anthony/a20/RegisterActivity.java
+++ app/src/main/java/com/example/anthony/a20/RegisterActivity.java
@@ -1,21 +1,19 @@
 package com.example.anthony.a20;
 
-import android.Manifest;
-import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,12 +21,48 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.File;
+
 
 public class RegisterActivity extends AppCompatActivity {
     public static final int PICK_IMAGE = 1;
     private FirebaseAuth mAuth;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    protected static final int GALLERY_PICTURE = 1;
+    protected static final int CAMERA_REQUEST = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //Variables
+        mAuth = FirebaseAuth.getInstance();
+        ImageView img_profile = findViewById(R.id.img_profile);
+        Button btn_register = findViewById(R.id.btn_save);
+
+        //Funciones click
+        img_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startDialogImage();
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextEmail = findViewById(R.id.edt_Correo);
+                editTextPassword= findViewById(R.id.edt_Contraseña);
+                final String email = editTextEmail.getText().toString();
+                final String password = editTextPassword.getText().toString();
+                Log.d("login", "signIn:" + email);
+                createAccount(email,password);
+            }
+        });
+
+    }
+
     private void createAccount(String email, String password) {
         Log.d("login", "signIn:" + email);
         //if (!validateForm()) {
@@ -59,35 +93,48 @@ public class RegisterActivity extends AppCompatActivity {
         // [END sign_in_with_email]
     }
 
+    private void startDialogImage() {
+        AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(
+                this);
+        myAlertDialog.setTitle("Foto de Perfil");
+        myAlertDialog.setMessage("Que desea abrir?");
+        myAlertDialog.setPositiveButton("Galeria",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent pictureActionIntent = null;
+                        pictureActionIntent = new Intent(
+                                Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(
+                                pictureActionIntent,
+                                GALLERY_PICTURE);
+
+                    }
+                });
+
+        myAlertDialog.setNegativeButton("Camara",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent intent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        File f = new File(android.os.Environment
+                                .getExternalStorageDirectory(), "temp.jpg");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(f));
+
+                        startActivityForResult(intent,
+                                CAMERA_REQUEST);
+
+                    }
+                });
+        myAlertDialog.show();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        ImageView img_profile = findViewById(R.id.img_profile);
-        Button btn_save = findViewById(R.id.btn_save);
-        mAuth = FirebaseAuth.getInstance();
-        btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editTextEmail = findViewById(R.id.edt_Correo);
-                editTextPassword= findViewById(R.id.edt_Contraseña);
-                final String email = editTextEmail.getText().toString();
-                final String password = editTextPassword.getText().toString();
-                Log.d("login", "signIn:" + email);
-                createAccount(email,password);
-            }
-        });
-        img_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-            }
-        });
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
+
     /*
     private boolean validateForm() {
         boolean valid = true;
